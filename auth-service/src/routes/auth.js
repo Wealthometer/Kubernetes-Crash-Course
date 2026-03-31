9f70c911ae87f2cc23c6445e973b533f02104586
Wealthometer <wealthhajoh87@gmail.com>
2026-03-31 12:06:02 +0100

    Update auth.js

--- auth-service/src/routes/auth.js
+++ auth-service/src/routes/auth.js
@@ -9,3 +9,4 @@ router.post('/register', async (req, res) => {
     const { email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 12);
     const user = new User({ email, password: hashedPassword });
+    await user.save();
