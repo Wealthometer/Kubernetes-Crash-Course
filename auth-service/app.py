4f47f37e22b284ff87b88a286c9b502cd6026aa6
Wealthometer <wealthhajoh87@gmail.com>
2026-03-31 12:01:11 +0100

    Update app.py

--- auth-service/app.py
+++ auth-service/app.py
@@ -25,3 +25,4 @@ def connect_to_db():
     if not all([db_user, db_password, db_host, db_name]):
         raise Exception("Database environment variables are not fully set.")
 
+    database_url = f"postgresql://{db_user}:{db_password}@{db_host}:5432/{db_name}"
