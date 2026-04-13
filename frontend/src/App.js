72b120bbb4c7acf39217955aef5cb6222304ee05
Wealthometer <wealthhajoh87@gmail.com>
2026-04-14 00:58:20 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,3 +2,4 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch, useHistory } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
+import Signup from './components/Signup';
