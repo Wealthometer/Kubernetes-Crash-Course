685682230effc3f5f60dc86d51ae4390c7e31d40
Wealthometer <wealthhajoh87@gmail.com>
2026-04-14 01:29:49 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -94,3 +94,4 @@ const Dashboard = () => {
                   <td className="py-2 px-4 text-center">{score.rps_wins}</td>
                   <td className="py-2 px-4 text-center">{score.rps_losses}</td>
                   <td className="py-2 px-4 text-center">{score.memory_game_wins || 0}</td>
+                  <td className="py-2 px-4 text-center">{score.memory_game_losses || 0}</td>
