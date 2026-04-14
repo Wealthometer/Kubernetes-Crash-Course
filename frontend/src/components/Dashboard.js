6ecdc60821091b15cd83ad7b3c54398dd511c259
Wealthometer <wealthhajoh87@gmail.com>
2026-04-14 01:20:58 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -6,3 +6,4 @@ import logo from '../assets/kubesimplify-logo.png';
 const Dashboard = () => {
   const [scores, setScores] = useState([]);
   const [username, setUsername] = useState('');
+  const history = useHistory();
