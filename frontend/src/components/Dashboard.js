557ae7f726910190471bff9c14286e734e57a82d
Wealthometer <wealthhajoh87@gmail.com>
2026-04-14 01:26:22 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -66,3 +66,4 @@ const Dashboard = () => {
               className="block mx-auto bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition duration-300 mb-6"
             >
               Logout
+            </button>
