aab42cab6d7d927e47c7c1101a4f60fe111c0ff5
Wealthometer <wealthhajoh87@gmail.com>
2026-04-14 01:20:51 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,4 +3,5 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 import logo from '../assets/kubesimplify-logo.png';
 
+const Dashboard = () => {
   const [scores, setScores] = useState([]);
