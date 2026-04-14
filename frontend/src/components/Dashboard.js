664ac7afda7ca228a2b0ada9bcc34c20592ffb77
Wealthometer <wealthhajoh87@gmail.com>
2026-04-14 01:29:06 +0100

    Update Dashboard.js

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -83,3 +83,4 @@ const Dashboard = () => {
                 <th className="py-2 px-4">MEMORY WINS</th>
                 <th className="py-2 px-4">MEMORY LOSSES</th>
               </tr>
+            </thead>
