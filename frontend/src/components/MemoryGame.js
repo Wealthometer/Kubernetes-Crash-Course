4ba3bf09dd2e9aef3bbb53a1fe166677c2581659
Wealthometer <wealthhajoh87@gmail.com>
2026-04-15 00:54:15 +0100

    Update MemoryGame.js

--- frontend/src/components/MemoryGame.js
+++ frontend/src/components/MemoryGame.js
@@ -23,6 +23,7 @@ const MemoryGame = () => {
       gameCards.push(
         { id: index * 2, symbol, isFlipped: false, isMatched: false },
       );
+    });
 
     for (let i = gameCards.length - 1; i > 0; i--) {
       [gameCards[i], gameCards[j]] = [gameCards[j], gameCards[i]];
